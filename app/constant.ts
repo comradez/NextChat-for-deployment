80e1da9df46554083435da5cf16202a7b6833ce7
Chuyan Zhang <chuyan@ucsb.edu>
2024-12-30 14:24:55 -0800

    fix model list - use deployment list to filter instead

--- app/constant.ts
+++ app/constant.ts
@@ -261,7 +261,7 @@ You are ChatGPT, a large language model trained by {{ServiceProvider}}.
 Knowledge cutoff: {{cutoff}}
 Current model: {{model}}
 Current time: {{time}}
-Latex inline: \\(x^2\\) 
+Latex inline: \\(x^2\\)
 Latex block: $$e=mc^2$$
 `;
 
@@ -323,140 +323,137 @@ export const VISION_MODEL_REGEXES = [
 export const EXCLUDE_VISION_MODEL_REGEXES = [/claude-3-5-haiku-20241022/];
 
 const openaiModels = [
-  // "gpt-3.5-turbo",
-  // "gpt-3.5-turbo-1106",
-  // "gpt-3.5-turbo-0125",
-  // "gpt-4",
-  // "gpt-4-0613",
+  "gpt-3.5-turbo",
+  "gpt-3.5-turbo-1106",
+  "gpt-3.5-turbo-0125",
+  "gpt-4",
+  "gpt-4-0613",
   "gpt-4-32k",
-  // "gpt-4-32k-0613",
+  "gpt-4-32k-0613",
   "gpt-4-turbo",
-  // "gpt-4-turbo-preview",
-  // "gpt-4o",
-  // "gpt-4o-2024-05-13",
-  // "gpt-4o-2024-08-06",
-  // "gpt-4o-2024-11-20",
+  "gpt-4-turbo-preview",
+  "gpt-4o",
+  "gpt-4o-2024-05-13",
+  "gpt-4o-2024-08-06",
+  "gpt-4o-2024-11-20",
   "chatgpt-4o-latest",
   "gpt-4o-mini",
-  // "gpt-4o-mini-2024-07-18",
-  // "gpt-4-vision-preview",
-  // "gpt-4-turbo-2024-04-09",
-  // "gpt-4-1106-preview",
-  // "dall-e-3",
+  "gpt-4o-mini-2024-07-18",
+  "gpt-4-vision-preview",
+  "gpt-4-turbo-2024-04-09",
+  "gpt-4-1106-preview",
+  "dall-e-3",
   "o1-mini",
   "o1-preview",
 ];
 
-const googleModels: string[] = [
-  // "gemini-1.0-pro", // Deprecated on 2/15/2025
-  // "gemini-1.5-pro-latest",
-  // "gemini-1.5-pro",
-  // "gemini-1.5-pro-002",
-  // "gemini-1.5-pro-exp-0827",
-  // "gemini-1.5-flash-latest",
-  // "gemini-1.5-flash-8b-latest",
-  // "gemini-1.5-flash",
-  // "gemini-1.5-flash-8b",
-  // "gemini-1.5-flash-002",
-  // "gemini-1.5-flash-exp-0827",
-  // "learnlm-1.5-pro-experimental",
-  // "gemini-exp-1114",
-  // "gemini-exp-1121",
-  // "gemini-exp-1206",
+const googleModels = [
+  "gemini-1.0-pro", // Deprecated on 2/15/2025
+  "gemini-1.5-pro-latest",
+  "gemini-1.5-pro",
+  "gemini-1.5-pro-002",
+  "gemini-1.5-pro-exp-0827",
+  "gemini-1.5-flash-latest",
+  "gemini-1.5-flash-8b-latest",
+  "gemini-1.5-flash",
+  "gemini-1.5-flash-8b",
+  "gemini-1.5-flash-002",
+  "gemini-1.5-flash-exp-0827",
+  "learnlm-1.5-pro-experimental",
+  "gemini-exp-1114",
+  "gemini-exp-1121",
+  "gemini-exp-1206",
   "gemini-2.0-flash-exp",
-  // "gemini-2.0-flash-thinking-exp-1219",
+  "gemini-2.0-flash-thinking-exp-1219",
 ];
 
 const anthropicModels = [
-  // "claude-instant-1.2",
-  // "claude-2.0",
-  // "claude-2.1",
-  // "claude-3-sonnet-20240229",
-  // "claude-3-opus-20240229",
-  // "claude-3-opus-latest",
-  // "claude-3-haiku-20240307",
-  // "claude-3-5-haiku-20241022",
+  "claude-instant-1.2",
+  "claude-2.0",
+  "claude-2.1",
+  "claude-3-sonnet-20240229",
+  "claude-3-opus-20240229",
+  "claude-3-opus-latest",
+  "claude-3-haiku-20240307",
+  "claude-3-5-haiku-20241022",
   "claude-3-5-haiku-latest",
-  // "claude-3-5-sonnet-20240620",
-  // "claude-3-5-sonnet-20241022",
+  "claude-3-5-sonnet-20240620",
+  "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-latest",
 ];
 
-const baiduModels: string[] = [
-  // "ernie-4.0-turbo-8k",
-  // "ernie-4.0-8k",
-  // "ernie-4.0-8k-preview",
-  // "ernie-4.0-8k-preview-0518",
-  // "ernie-4.0-8k-latest",
-  // "ernie-3.5-8k",
-  // "ernie-3.5-8k-0205",
-  // "ernie-speed-128k",
-  // "ernie-speed-8k",
-  // "ernie-lite-8k",
-  // "ernie-tiny-8k",
+const baiduModels = [
+  "ernie-4.0-turbo-8k",
+  "ernie-4.0-8k",
+  "ernie-4.0-8k-preview",
+  "ernie-4.0-8k-preview-0518",
+  "ernie-4.0-8k-latest",
+  "ernie-3.5-8k",
+  "ernie-3.5-8k-0205",
+  "ernie-speed-128k",
+  "ernie-speed-8k",
+  "ernie-lite-8k",
+  "ernie-tiny-8k",
 ];
 
-const bytedanceModels: string[] = [
-  // "Doubao-lite-4k",
-  // "Doubao-lite-32k",
-  // "Doubao-lite-128k",
-  // "Doubao-pro-4k",
-  // "Doubao-pro-32k",
-  // "Doubao-pro-128k",
+const bytedanceModels = [
+  "Doubao-lite-4k",
+  "Doubao-lite-32k",
+  "Doubao-lite-128k",
+  "Doubao-pro-4k",
+  "Doubao-pro-32k",
+  "Doubao-pro-128k",
 ];
 
-const alibabaModes: string[] = [
-  // "qwen-turbo",
-  // "qwen-plus",
-  // "qwen-max",
-  // "qwen-max-0428",
-  // "qwen-max-0403",
-  // "qwen-max-0107",
-  // "qwen-max-longcontext",
+const alibabaModes = [
+  "qwen-turbo",
+  "qwen-plus",
+  "qwen-max",
+  "qwen-max-0428",
+  "qwen-max-0403",
+  "qwen-max-0107",
+  "qwen-max-longcontext",
 ];
 
-const tencentModels: string[] = [
-  // "hunyuan-pro",
-  // "hunyuan-standard",
-  // "hunyuan-lite",
-  // "hunyuan-role",
-  // "hunyuan-functioncall",
-  // "hunyuan-code",
-  // "hunyuan-vision",
+const tencentModels = [
+  "hunyuan-pro",
+  "hunyuan-standard",
+  "hunyuan-lite",
+  "hunyuan-role",
+  "hunyuan-functioncall",
+  "hunyuan-code",
+  "hunyuan-vision",
 ];
 
-const moonshotModes: string[] = [
-  // "moonshot-v1-8k", "moonshot-v1-32k", "moonshot-v1-128k"
+const moonshotModes = ["moonshot-v1-8k", "moonshot-v1-32k", "moonshot-v1-128k"];
+
+const iflytekModels = [
+  "general",
+  "generalv3",
+  "pro-128k",
+  "generalv3.5",
+  "4.0Ultra",
 ];
 
-const iflytekModels: string[] = [
-  // "general",
-  // "generalv3",
-  // "pro-128k",
-  // "generalv3.5",
-  // "4.0Ultra",
-];
 const deepseekModels = ["deepseek-chat", "deepseek-coder"];
 
-const xAIModes: string[] = [
-  // "grok-beta"
-];
+const xAIModes = ["grok-beta"];
 
-const chatglmModels: string[] = [
-  // "glm-4-plus",
-  // "glm-4-0520",
-  // "glm-4",
-  // "glm-4-air",
-  // "glm-4-airx",
-  // "glm-4-long",
-  // "glm-4-flashx",
-  // "glm-4-flash",
-  // "glm-4v-plus",
-  // "glm-4v",
-  // "glm-4v-flash", // free
-  // "cogview-3-plus",
-  // "cogview-3",
-  // "cogview-3-flash", // free
+const chatglmModels = [
+  "glm-4-plus",
+  "glm-4-0520",
+  "glm-4",
+  "glm-4-air",
+  "glm-4-airx",
+  "glm-4-long",
+  "glm-4-flashx",
+  "glm-4-flash",
+  "glm-4v-plus",
+  "glm-4v",
+  "glm-4v-flash", // free
+  "cogview-3-plus",
+  "cogview-3",
+  "cogview-3-flash", // free
   // 目前无法适配轮询任务
   //   "cogvideox",
   //   "cogvideox-flash", // free
@@ -475,6 +472,17 @@ export const DEFAULT_MODELS = [
       sorted: 1, // 这里是固定的，确保顺序与之前内置的版本一致
     },
   })),
+  ...openaiModels.map((name) => ({
+    name,
+    available: true,
+    sorted: seq++,
+    provider: {
+      id: "azure",
+      providerName: "Azure",
+      providerType: "azure",
+      sorted: 2,
+    },
+  })),
   ...googleModels.map((name) => ({
     name,
     available: true,
@@ -483,7 +491,7 @@ export const DEFAULT_MODELS = [
       id: "google",
       providerName: "Google",
       providerType: "google",
-      sorted: 2,
+      sorted: 3,
     },
   })),
   ...anthropicModels.map((name) => ({
@@ -494,7 +502,7 @@ export const DEFAULT_MODELS = [
       id: "anthropic",
       providerName: "Anthropic",
       providerType: "anthropic",
-      sorted: 3,
+      sorted: 4,
     },
   })),
   ...baiduModels.map((name) => ({
@@ -505,7 +513,7 @@ export const DEFAULT_MODELS = [
       id: "baidu",
       providerName: "Baidu",
       providerType: "baidu",
-      sorted: 4,
+      sorted: 5,
     },
   })),
   ...bytedanceModels.map((name) => ({
@@ -516,7 +524,7 @@ export const DEFAULT_MODELS = [
       id: "bytedance",
       providerName: "ByteDance",
       providerType: "bytedance",
-      sorted: 5,
+      sorted: 6,
     },
   })),
   ...alibabaModes.map((name) => ({
@@ -527,7 +535,7 @@ export const DEFAULT_MODELS = [
       id: "alibaba",
       providerName: "Alibaba",
       providerType: "alibaba",
-      sorted: 6,
+      sorted: 7,
     },
   })),
   ...tencentModels.map((name) => ({
@@ -538,7 +546,7 @@ export const DEFAULT_MODELS = [
       id: "tencent",
       providerName: "Tencent",
       providerType: "tencent",
-      sorted: 7,
+      sorted: 8,
     },
   })),
   ...moonshotModes.map((name) => ({
@@ -549,7 +557,7 @@ export const DEFAULT_MODELS = [
       id: "moonshot",
       providerName: "Moonshot",
       providerType: "moonshot",
-      sorted: 8,
+      sorted: 9,
     },
   })),
   ...iflytekModels.map((name) => ({
@@ -560,7 +568,7 @@ export const DEFAULT_MODELS = [
       id: "iflytek",
       providerName: "Iflytek",
       providerType: "iflytek",
-      sorted: 9,
+      sorted: 10,
     },
   })),
   ...xAIModes.map((name) => ({
@@ -571,7 +579,7 @@ export const DEFAULT_MODELS = [
       id: "xai",
       providerName: "XAI",
       providerType: "xai",
-      sorted: 10,
+      sorted: 11,
     },
   })),
   ...chatglmModels.map((name) => ({
@@ -582,7 +590,7 @@ export const DEFAULT_MODELS = [
       id: "chatglm",
       providerName: "ChatGLM",
       providerType: "chatglm",
-      sorted: 11,
+      sorted: 12,
     },
   })),
   ...deepseekModels.map((name) => ({
@@ -593,7 +601,7 @@ export const DEFAULT_MODELS = [
       id: "deepseek",
       providerName: "DeepSeek",
       providerType: "deepseek",
-      sorted: 12,
+      sorted: 13,
     },
   })),
 ] as const;
@@ -618,3 +626,4 @@ export const DEFAULT_GA_ID = "G-89WN60ZK2E";
 
 export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
 export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+
